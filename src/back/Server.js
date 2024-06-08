f619175d4973671ec058ec1415c15badada6fe2e
BrayanC190 <L19021058@veracruz.tecnm.mx>
2024-06-07 22:47:51 -0600

    Comentarios

--- src/back/Server.js
+++ src/back/Server.js
@@ -17,7 +17,7 @@ const MetodosGet = require('./MetodosGet'); // Selects
 const MetodosPost = require('./MetodosPost'); //Inserts
 const MetodosPut = require('./MetodosPut'); //Update
 
-// Define routes using controllers
+
 app.get('/tabla/:tableName', MetodosGet.getTabla);
 app.get('/tablaReferencia', MetodosGet.getTablaReferencia);
 app.get('/VerTablaOperacion', MetodosGet.getVerTablaOperacion);
@@ -32,7 +32,7 @@ app.post('/Aumentoestatus', MetodosPost.aumentoEstatus);
 
 app.put('/UpdateOperacion', MetodosPut.updateOperacion);
 
-// Start server
+
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}/`);
 });
